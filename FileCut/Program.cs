c9f9a679976b1b78feef151b9cb37da5c2b1ec6a
Xairooo <Xairooo@users.noreply.github.com>
2023-09-09 12:08:21 +0200

    Projektdateien hinzufügen.

---
+++ FileCut/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileCut;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Check if command-line arguments are provided
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Drag and drop the file to cut on this app.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            return;
+        }
+
+        // Get the file name from the command-line argument
+        var fileName = args[0];
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine($"The file {fileName} does not exist");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            return;
+        }
+        Console.WriteLine(fileName);
+        Console.WriteLine("Cut position? (in %)");
+
+        int splitP = 0;
+        while (splitP == 0)
+        {
+            // Read and parse the user input for the cut position
+            int.TryParse(Console.ReadLine(), out splitP);
+            Console.WriteLine("Incorrect input!");
+        }
+
+        // Get information about the input file
+        var fileInfo = new FileInfo(fileName);
+        long fileLengthTarget = (fileInfo.Length / 100) * splitP;
+        var fileExt = Path.GetExtension(fileName);
+        var cnt = 1;
+        string targetFileName = fileInfo.Name.Replace(fileExt, $"-{cnt}{fileExt}");
+
+        // Ensure that the target file name is unique
+        while (File.Exists(targetFileName))
+        {
+            targetFileName = fileInfo.Name.Replace(fileExt, $"-{cnt}{fileExt}");
+            cnt++;
+        }
+
+        Console.WriteLine($"New File Name: {targetFileName}");
+
+        // Open the input and output file streams for copying data
+        using (var fsRead = new FileStream(fileName, FileMode.Open))
+        using (var fsWrite = new FileStream(targetFileName, FileMode.Create))
+        {
+            while (fsRead.Position < fileLengthTarget)
+            {
+                // Copy data byte by byte from the input to the output file
+                fsWrite.WriteByte((byte)fsRead.ReadByte());
+            }
+
+            // Close the file streams
+            fsWrite.Close();
+            fsRead.Close();
+
+            // Display success message with the size of the cut file
+            Console.WriteLine($"Success {helper.ConvertBytesToPrettyString(fileLengthTarget)} written.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FileCut/helper.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCut;
+
+internal static class helper
+{
+    public static double KilobytesToBytes(double input) => (input * 1024f);
+    public static double MegabytesToBytes(double input) => ((input * 1024f) * 1024f);
+    public static double GigabytesToBytes(double input) => (((input * 1024f) * 1024f) * 1024F);
+    public static double TerabytesToBytes(double input) => ((((input * 1024f) * 1024f) * 1024F) * 1024F);
+
+    public static double BytesToTerabytes(double bytes) => ((((bytes / 1024f) / 1024f) / 1024f) / 1024f);
+    public static double BytesToGigabytes(double bytes) => (((bytes / 1024f) / 1024f) / 1024f);
+    public static double BytesToMegabytes(double bytes) => ((bytes / 1024f) / 1024f);
+    public static double BytesToKilobytes(double bytes) => (bytes / 1024f);
+
+    public static string BytesToPrettyString(object Size) => ConvertBytesToPrettyString((float)Convert.ToDouble(Size));
+    public static string BytesToPrettyString(double Size) => ConvertBytesToPrettyString((float)Size);
+    public static string BytesToPrettyString(float Size) => ConvertBytesToPrettyString(Size);
+    public static string BytesToPrettyString(int Size) => ConvertBytesToPrettyString(Size);
+
+    internal static string ConvertBytesToPrettyString(float Size, int count = 0)
+    {
+        float F = Size / 1024f;
+        if (F < 1)
+        {
+            switch (count)
+            {
+                case 0:
+                    return $"{Size:0.00} byte";
+                case 1:
+                    return $"{Size:0.00} kb";
+                case 2:
+                    return $"{Size:0.00} mb";
+                case 3:
+                    return $"{Size:0.00} gb";
+                case 4:
+                    return $"{Size:0.00} tb";
+                case 5:
+                    return $"{Size:0.00} pb";
+                case 6:
+                    return $"{Size:0.00} eb";
+                case 7:
+                    return $"{Size:0.00} zb";
+                case 8:
+                    return $"{Size:0.00} yb";
+            }
+        }
+        return ConvertBytesToPrettyString(F, ++count);
+    }
+}
